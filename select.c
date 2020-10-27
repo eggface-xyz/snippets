e95662fda10080511238259b9b5dfb2b45560a7f
chuansun <Chuanqi.Sun@amd.com>
2020-10-27 17:29:44 +0800

    [OSS] Add select demo

---
+++ select.c
@@ -0,0 +1,70 @@
+#include <select.h>
+
+#define LOCAL_HOST  "127.0.0.1"
+#define BUF_SIZE    1024
+
+int startserver(int port)
+{
+    int res;
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(listen_fd < 0) {
+        printf("socket() error: %s\n", strerror(errno));
+        return errno;
+    }
+
+    const struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = port;
+    inet_aton(LOCAL_HOST, &addr.sin_addr);
+    res = bind(listen_fd, addr, sizeof(addr));
+    if(res < 0) {
+        printf("bind() error: %s\n", strerror(errno));
+        goto fail;
+    }
+
+    res = listen(listen_fd, 5);
+    if(res < 0) {
+        printf("listen() error: %s\n", strerror(errno));
+        goto fail;
+    }
+
+    fd_set readfds;
+    FD_SET(listen_fd, &readfds);
+    int nfds = listen_fd + 1;
+    while(true) {
+        int cnt = select(nfds, &readfds, NULL, NULL, NULL);
+        if(cnt < 0) {
+            printf("accept error: %s\n", strerror(errno));
+            goto fail;
+        }
+        if(IS_FDSET(listen_fd, readfds)) {
+            printf("process connection request\n");
+            
+            struct sockaddr_in client_addr;
+            socklen_t len;
+            int client_fd = accept(listen_fd, &client_addr, &len);
+            if(client_fd < 0) {
+                printf("accept error: %s\n", strerror(errno));
+                goto fail;
+            }
+            FD_SET(client_fd, &readfds);
+            nfds = client_fd + 1;
+        } else {
+            printf("process data request\n");
+            uint8_t buffer[BUF_SIZE];
+        }
+    }
+
+    return 0;
+
+fail:
+    close(fd);
+    return errno;
+}
+
+int main()
+{
+    int res = startserver(port);
+    if(res < 0)
+        return 1;
+}
